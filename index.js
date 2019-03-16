4bdb6945ab00a79ec804ab9c96455d9b19b9765b
boltgolt <boltgolt@gmail.com>
2019-03-16 13:27:42 +0100

    Electro window working

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+const path = require("path")
 const electron = require("electron")
 const app = electron.app
 
 function createWindow () {
-  // Create the browser window.
-  let win = new electron.BrowserWindow({ width: 800, height: 600 })
+  let win = new electron.BrowserWindow({
+		width: 600,
+		height: 600,
+		frame: false,
+		resizable: true,
+		nodeIntegration: false,
+		icon: path.join(__dirname, "img/icon-64x64.png")
+  })
 
   // and load the index.html of the app.
   win.loadFile("index.html")
