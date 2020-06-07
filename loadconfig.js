c629c80014a201074ca86e24bc7afa4ca1e32c41
boltgolt <boltgolt@gmail.com>
2020-06-07 17:45:07 +0200

    Fixed align issues, redid molly code, realign nuke an train

--- loadconfig.js
+++ loadconfig.js
@@ -2,10 +2,15 @@ const fs = require("fs")
 const JSON5 = require("json5")
 const path = require("path")
 const util = require("util")
+const pack = require("./package.json")
 
 let cachedConfig = false
 // Load the default config
 let loadedConfig = JSON5.parse(fs.readFileSync(path.join(__dirname, "config", "config.json5"), "utf8"))
+
+if (pack.version != loadedConfig._version) {
+	console.warn(`WARNING config.json version missmatch (radar v${pack.version}, config v${loadedConfig._version})`)
+}
 
 // If there is an overwrite config file
 if (fs.existsSync(path.join(__dirname, "config", "config.override.json5"))) {
